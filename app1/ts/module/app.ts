058ce8d90e8ccc141b5ecda438866bd02aed6356
Renato Xavier <59875607+renatoxdsr@users.noreply.github.com>
2021-10-31 15:23:38 -0300

    Create app.ts

---
+++ app1/ts/module/app.ts
@@ -0,0 +1,15 @@
+let clienteController = new ClienteController();
+
+clienteController.listar();
+
+const c1 = new Cliente('Valenço', '10036224599', '269958');
+//const p1 = new Poupanca('2', 100);
+//const cb1 = new ContaBonificada('3', 0);
+
+console.log('Conta: ' + c1.nome);
+console.log('Conta: ' + c1.cpf);
+
+//p1.atualizarConta();
+//console.log('Conta: ' + p1.conta);
+
+
